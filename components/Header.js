88efd09c4ce14c5c781d5b67b95b6e4ed96a6509
Thomas Alemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-07-10 15:07:34 +0300

    Navbar

---
+++ components/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import Image from "next/image";
+
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from "@heroicons/react/solid";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* Image */}
+      <div className="relative flex items-center h-10 cursor-pointer">
+        <Image
+          src="/images/Airbnb-Logo.png"
+          alt="Airbnb logo"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* Search */}
+      <div className="flex items-center border-2 rounded-full py-1 md:shadow-sm ">
+        <input
+          type="text"
+          className=" flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-airbnbRed text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+
+      {/* Nav */}
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline-flex cursor-pointer pl-1">
+          Become a host
+        </p>
+        <GlobeAltIcon className="h-6" />
+        <div className="flex items-center space-x-2 border-2 px-2 py-1 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
