a6fcd3786c59c5dc765b198f58b456e278201115
Thomas Alemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-07-10 15:49:15 +0300

    Small cards

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div>
       <Head>
@@ -12,11 +13,86 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header />
+      <Header />
 
-        <Banner />
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        {/* Small Cards */}
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+
+        {/* Medium Sections */}
+        <section>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, location, distance }, index) => (
+              <h1 key={index}>
+                {
+                  <SmallCard
+                    image={img}
+                    location={location}
+                    distance={distance}
+                  />
+                }
+              </h1>
+            ))}
+          </div>
+        </section>
+
+        {/* Large Cards */}
+        <section></section>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = [
+    {
+      img: "https://links.papareact.com/5j2",
+      location: "London",
+      distance: "45-minute drive",
+    },
+    {
+      img: "https://links.papareact.com/1to",
+      location: "Manchester",
+      distance: "4.5-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/40m",
+      location: "Liverpool",
+      distance: "4.5-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/msp",
+      location: "York",
+      distance: "4-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/2k3",
+      location: "Cardiff",
+      distance: "45-minute drive",
+    },
+    {
+      img: "https://links.papareact.com/ynx",
+      location: "Birkenhead",
+      distance: "4.5-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/kji",
+      location: "Newquay",
+      distance: "6-hour drive",
+    },
+    {
+      img: "https://links.papareact.com/41m",
+      location: "Hove",
+      distance: "2-hour drive",
+    },
+  ];
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
