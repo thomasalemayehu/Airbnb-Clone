6383491ad37b04d4d5f81b847615f9c9d458d465
Thomas Alemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-07-13 10:33:54 +0300

    Footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,38 +6,38 @@ function Footer() {
       {/*  */}
       <div className="space-y-4 text-xs text-gray-500">
         <h5 className="font-bold">ABOUT</h5>
-        <p>How Airbnb works</p>
-        <p>Newsroom</p>
-        <p>Investors</p>
-        <p>Airbnb Plus</p>
-        <p>Airbnb Luxe</p>
+        <p className="footer-link">How Airbnb works</p>
+        <p className="footer-link">Newsroom</p>
+        <p className="footer-link">Investors</p>
+        <p className="footer-link">Airbnb Plus</p>
+        <p className="footer-link">Airbnb Luxe</p>
       </div>
       {/*  */}
       <div className="space-y-4 text-xs text-gray-500">
         <h5 className="font-bold">COMMUNITY</h5>
-        <p>Accessibility</p>
-        <p>Spoiler Not a real site</p>
-        <p>Its pretty site</p>
-        <p>Referrals accepted</p>
-        <p>Thomas Alemayehu</p>
+        <p className="footer-link">Accessibility</p>
+        <p className="footer-link">Spoiler Not a real site</p>
+        <p className="footer-link">Its pretty site</p>
+        <p className="footer-link">Referrals accepted</p>
+        <p className="footer-link">Thomas Alemayehu</p>
       </div>
       {/*  */}
       <div className="space-y-4 text-xs text-gray-500">
         <h5 className="font-bold">HOST</h5>
-        <p>Thomas Alemayehu</p>
-        <p>Presents</p>
-        <p>Full Stack Engineer</p>
-        <p>Mobile App Developer</p>
-        <p>Hire Now</p>
+        <p className="footer-link">Thomas Alemayehu</p>
+        <p className="footer-link">Presents</p>
+        <p className="footer-link">Full Stack Engineer</p>
+        <p className="footer-link">Mobile App Developer</p>
+        <p className="footer-link">Hire Now</p>
       </div>
       {/*  */}
       <div className="space-y-4 text-xs text-gray-500">
         <h5 className="font-bold">SUPPORT</h5>
-        <p>Help Center</p>
-        <p>Trust & Safety</p>
-        <p>Say Hi YouTube</p>
-        <p>Easter Eggs</p>
-        <p>For the Win</p>
+        <p className="footer-link">Help Center</p>
+        <p className="footer-link">Trust & Safety</p>
+        <p className="footer-link">Say Hi YouTube</p>
+        <p className="footer-link">Easter Eggs</p>
+        <p className="footer-link">For the Win</p>
       </div>
     </div>
   );
