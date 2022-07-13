67e1a062e46a67da8b6788a62a72ecd108b5e1e7
Thomas Alemayehu <61502533+thomasalemayehu@users.noreply.github.com>
2022-07-13 15:22:59 +0300

    Auth Page UI

--- components/SignInButton.js
+++ components/SignInButton.js
@@ -9,21 +9,23 @@ function SignInButton({ providerName, providerImage, providerId }) {
   return (
     <>
       {showLoader ? (
-        <div className="flex items-center space-x-3 mb-6 border border-gray-100 px-3 py-2 transition duration-200 ease-out hover:bg-primary active:scale-95 hover:shadow-lg active:text-white rounded-md">
+        <div className="flex items-center space-x-3 mb-6 border border-gray-100 px-3 py-2 transition duration-200 ease-out hover:bg-primary active:scale-95 hover:shadow-lg active:text-white rounded-md min-w-[200px] max-w-[320px] md:max-w-[400px] md:py-3">
           <Spinner />
         </div>
       ) : (
         <div
-          className="flex items-center space-x-3 mb-6 border border-primary px-3 py-2 transition duration-200 ease-out hover:bg-primary active:scale-95 hover:shadow-lg active:text-white rounded-md"
+          className="flex items-center space-x-3 mb-6 border border-primary px-3 py-2 transition duration-200 ease-out hover:bg-primary active:scale-95 hover:shadow-lg active:text-white rounded-md min-w-[200px] max-w-[320px] md:max-w-[400px] md:py-3"
           onClick={() => {
             signIn(providerId);
             setShowLoader(true);
           }}
         >
-          <div className="relative w-6 h-6">
+          <div className="relative w-6 h-6 hidden md:inline-block lg:w-8 lg:h-8">
             <Image src={providerImage} alt={providerName} layout="fill" />
           </div>
-          <div>Sign in With {providerName}</div>
+          <div className="text-base md:text-lg font-semibold">
+            Sign in With {providerName}
+          </div>
         </div>
       )}
     </>
--- pages/auth.js
+++ pages/auth.js
@@ -1,27 +1,56 @@
 import React from "react";
 import CustomHead from "../components/CustomHead";
-
 import { getProviders, signIn } from "next-auth/react";
 import SignInButton from "../components/SignInButton";
 import Header from "../components/Header";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 function Auth({ providers }) {
+  const router = useRouter();
   return (
     <>
       <CustomHead pageTitle={"Airbnb - Auth"}></CustomHead>
-      <Header />
-      <div>
-        <main className="flex flex-col max-w-screen-sm px-8 md:px-48 mx-auto my-32 bg-gray-200 py-8">
-          {Object?.values(providers).map(({ name, id }) => (
+
+      <main className="h-screen w-screen  flex">
+        {/* lEft */}
+        <div className="hidden lg:inline-block w-[55%] h-screen bg-primary px-8 py-6 pt-12">
+          <h1 className="text-black text-5xl font-bold mb-8">Airbnb</h1>
+          <div className="relative w-5/6 h-5/6">
+            <Image
+              src="/images/Auth.svg"
+              alt="Airbnb logo"
+              layout="fill"
+              objectFit="contain"
+              objectPosition="left"
+            />
+          </div>
+        </div>
+
+        {/* right */}
+        <div className=" w-[100%]  lg:w-[45%] h-screen  px-8 flex  flex-col justify-center">
+          <div
+            className="relative flex items-center  h-24 cursor-pointer mb-10"
+            onClick={() => router.push("/")}
+          >
+            <Image
+              src="/images/Airbnb-Logo.png"
+              alt="Airbnb logo"
+              layout="fill"
+              objectFit="contain"
+              objectPosition="left"
+            />
+          </div>
+          {Object.values(providers).map(({ name, id }) => (
             <SignInButton
               key={id}
               providerName={name}
               providerId={id}
-              providerImage={`/icons/${name}.svg`}
+              providerImage={`icons/${name}.svg`}
             />
           ))}
-        </main>
-      </div>
+        </div>
+      </main>
     </>
   );
 }
